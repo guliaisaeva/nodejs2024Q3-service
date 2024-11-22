e71fb3acd7167964201abd81c0936a2506a618b9
Gulia <gulia.isaeva.pls@gmail.com>
2024-11-22 11:14:42 +0300

    init: start loggin and athenthication/authorization task

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import 'dotenv/config';
+import * as yaml from 'yamljs';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const doc: OpenAPIObject = yaml.load('./doc/api.yaml');
+  SwaggerModule.setup('doc', app, doc);
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
